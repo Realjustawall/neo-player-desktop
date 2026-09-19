a434595d744002d3c2146718e9e15d3388bfe727
JustAWall <intesho.office@gmail.com>
2026-09-19 21:20:30 +0330

    frontend: activate enhanced offline player UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import PlayerApp from './PlayerApp'
 import './styles.css'
+import './enhancements.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PlayerApp />
   </React.StrictMode>,
 )
