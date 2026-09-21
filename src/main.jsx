2bfb9c773164d2c68feae5d6b22dba570f1141f7
JustAWall <intesho.office@gmail.com>
2026-09-21 06:26:19 +0330

    Lazy-load heavy tools and enable performance core

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@fontsource-variable/vazirmatn/wght.css'
 import PlayerApp from './PlayerApp'
-import FeatureHub from './FeatureHub'
+import FeatureHubLoader from './FeatureHubLoader'
 import ImmersiveHub from './ImmersiveHub'
 import LogoRepair from './LogoRepair'
 import LibraryBoost from './LibraryBoost'
+import PerformanceCore from './PerformanceCore'
 import './styles.css'
 import './enhancements.css'
 import './featurehub.css'
@@ -13,13 +14,15 @@ import './polish.css'
 import './immersive.css'
 import './core-settings.css'
 import './library-boost.css'
+import './runtime-performance.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PlayerApp />
-    <FeatureHub />
+    <FeatureHubLoader />
     <ImmersiveHub />
     <LogoRepair />
     <LibraryBoost />
+    <PerformanceCore />
   </React.StrictMode>,
 )
